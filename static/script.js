5a35f4248f486ac26e3d93f4ce37439de7688700
Dylan Snelgrove <45444843+Dylan987@users.noreply.github.com>
2019-09-14 17:26:46 -0400

    updated for flask
    hehe

---
+++ static/script.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  //add search functionality
+
+  $(".song-search").keyup(function(e) {
+      if(e.which == 13) {
+        let element = this;
+        let query = this.value;
+        console.log(query);
+        let safe_query = encodeURIComponent(query);
+
+
+        //do the ajax call
+        let server_url = "http://fb509cfe.ngrok.io/getData"; //TODO get the server_url
+        $.get(server_url+"?q="+safe_query, function(data) {
+            console.log(data);
+            ids = JSON.parse(data);
+
+            for (let i = 0; i < ids.length; i++){
+              element.append('<iframe src="https://open.spotify.com/embed/track/' + ids[i] +  'width="300" height="380" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
+            }
+
+          }, "json");
+
+      }
+  });
+});
