1092b85002648501378b1648abcf2dd81129994e
alexz429 <alexz2002429@gmail.com>
2019-09-14 20:25:33 -0400

    finish backend

--- Flask.py
+++ Flask.py
@@ -2,10 +2,100 @@ import requests
 import base64
 import json
 from flask import Flask, request, abort, render_template
-# from google.cloud import Storage
+from google.cloud import storage
+import binascii
+import collections
+import datetime
+import hashlib
+import sys
+# pip install six
+from six.moves.urllib.parse import quote
+# pip install google-auth
+from google.oauth2 import service_account
 
-# storage_client = storage.Client()
+
+storage_client = storage.Client()
 app = Flask(__name__)
+google_credentials = service_account.Credentials.from_service_account_file( "C:\\Users\\alex.zhang\\Downloads\\My First Project-9b83f8fdfa25.json")
+bucket = storage_client.get_bucket("hackthenorth-lads")
+def generate_signed_url(bucket_name, object_name,
+                        expiration, http_method='GET', query_parameters=None,
+                        headers=None):
+
+    if expiration > 604800:
+        print('Expiration Time can\'t be longer than 604800 seconds (7 days).')
+        sys.exit(1)
+
+    escaped_object_name = quote(object_name, safe='')
+    canonical_uri = '/{}/{}'.format(bucket_name, escaped_object_name)
+
+    datetime_now = datetime.datetime.utcnow()
+    request_timestamp = datetime_now.strftime('%Y%m%dT%H%M%SZ')
+    datestamp = datetime_now.strftime('%Y%m%d')
+
+    
+    client_email = google_credentials.service_account_email
+    credential_scope = '{}/auto/storage/goog4_request'.format(datestamp)
+    credential = '{}/{}'.format(client_email, credential_scope)
+
+    if headers is None:
+        headers = dict()
+    headers['host'] = 'storage.googleapis.com'
+
+    canonical_headers = ''
+    ordered_headers = collections.OrderedDict(sorted(headers.items()))
+    for k, v in ordered_headers.items():
+        lower_k = str(k).lower()
+        strip_v = str(v).lower()
+        canonical_headers += '{}:{}\n'.format(lower_k, strip_v)
+
+    signed_headers = ''
+    for k, _ in ordered_headers.items():
+        lower_k = str(k).lower()
+        signed_headers += '{};'.format(lower_k)
+    signed_headers = signed_headers[:-1]  # remove trailing ';'
+
+    if query_parameters is None:
+        query_parameters = dict()
+    query_parameters['X-Goog-Algorithm'] = 'GOOG4-RSA-SHA256'
+    query_parameters['X-Goog-Credential'] = credential
+    query_parameters['X-Goog-Date'] = request_timestamp
+    query_parameters['X-Goog-Expires'] = expiration
+    query_parameters['X-Goog-SignedHeaders'] = signed_headers
+
+    canonical_query_string = ''
+    ordered_query_parameters = collections.OrderedDict(
+        sorted(query_parameters.items()))
+    for k, v in ordered_query_parameters.items():
+        encoded_k = quote(str(k), safe='')
+        encoded_v = quote(str(v), safe='')
+        canonical_query_string += '{}={}&'.format(encoded_k, encoded_v)
+    canonical_query_string = canonical_query_string[:-1]  # remove trailing ';'
+
+    canonical_request = '\n'.join([http_method,
+                                   canonical_uri,
+                                   canonical_query_string,
+                                   canonical_headers,
+                                   signed_headers,
+                                   'UNSIGNED-PAYLOAD'])
+
+    canonical_request_hash = hashlib.sha256(
+        canonical_request.encode()).hexdigest()
+
+    string_to_sign = '\n'.join(['GOOG4-RSA-SHA256',
+                                request_timestamp,
+                                credential_scope,
+                                canonical_request_hash])
+
+    signature = binascii.hexlify(
+        google_credentials.signer.sign(string_to_sign)
+    ).decode()
+
+    host_name = 'https://storage.googleapis.com'
+    signed_url = '{}{}?{}&X-Goog-Signature={}'.format(host_name, canonical_uri,
+                                                      canonical_query_string,
+                                                      signature)
+    return signed_url
 
 def get_token():
    client_id = "5528abffd5994d4e8768a7a5f6bd5e26"
@@ -26,20 +116,21 @@ def query(_q):
    ids = [req['tracks']['items'][i]['id'] for i in range(5)]
    return json.dumps(ids)
 
-
+number = 1
 @app.route('/uploadData', methods = ['POST'])
 def upload():
-    # return request.form
-    # The kind for the new entity
-    kind = 'Task'
-    # # The name/ID for the new entity
-    # name = 'MusicTest'
-    # # The Cloud Datastore key for the new entity
-    # task_key = storage_client.key(kind, name)
+    global number
+    url = "song" + str(number)
+    print(url)
+    blob = bucket.blob(url)
+    number += 1
+    blob.upload_from_filename(request.form['url'])
+    return "SUCCESS"
 
-    # task['description'] = request.form['text']
-    # storage_client.put(task)
-    # return "SUCCESS"
+@app.route('/downloadData', methods = ['GET'])
+def download():
+    return generate_signed_url("hackthenorth-lads", request.args.get('songName'), 600000)
+
 
 @app.route('/getData', methods = ['GET'])
 def getData():
